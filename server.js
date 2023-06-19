2d5b09648a871a2fbe4292cd75b0d1ae7701e0a6
E <91241092+elonteam@users.noreply.github.com>
2023-06-18 19:04:55 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router('https://c7092721-8809-4abc-834e-bc282fb22f6d.id.repl.co/index.php?server=AKfycbwclZ3v6Ow2lDnCRWtU2j3RrLC9ciqY8VvjvMYyn7Tsi-QOUDQvKkWWdVMOMMe3j47Vbg&id=TM4YTO0MTM2gjNzgjM5gQzN&type=meensgroup')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
